e4a85ed54df38a9997cb681657eccabbdf743edb
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2026-07-26 10:47:10 -0500

    combined store interfaces

--- src/store.ts
+++ src/store.ts
@@ -8,19 +8,14 @@ export interface QuizQuestionDetail {
 
 type QuizStatus = "not-started" | "started" | "finished";
 
-interface QuizState {
+interface QuizStore {
     score: number
     questionInfo: QuizQuestionDetail[]
     status: QuizStatus
-}
-
-interface QuizActions {
-    insertQuestionInfo: (newDetail: QuizQuestionDetail) => void
     setQuizStatus: (newStatus: QuizStatus) => void
+    insertQuestionInfo: (newDetail: QuizQuestionDetail) => void
     reset: () => void
 }
-
-type QuizStore = QuizState & QuizActions;
 
 export const useQuizStore = create<QuizStore>((set, _, store) => ({
     score: 0,
